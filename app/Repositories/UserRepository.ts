547d14d54e3cccd15023188f90a6c03c6a3a055b
Felipe Feliciano <fehzl@live.com>
2022-02-17 00:43:36 +0000

    :sparkles: feat: add create method to UserRepository

--- app/Repositories/UserRepository.ts
+++ app/Repositories/UserRepository.ts
@@ -1,4 +1,5 @@
 import User from 'App/Models/User'
+import CreateUserValidator from 'App/Validators/User/CreateUserValidator'
 
 export default class UserRepository {
   private User: typeof User
@@ -8,6 +9,10 @@ export default class UserRepository {
   }
 
   public async getById(id: string): Promise<User | null> {
-    return await this.User.find(id)
+    return this.User.find(id)
+  }
+
+  public async create(data: CreateUserValidator['schema']['props']): Promise<User> {
+    return this.User.create(data)
   }
 }
