25577204a5066b74959fccc385e7f98d09a5a3fd
Felipe Feliciano <fehzl@live.com>
2022-02-17 01:47:54 +0000

    :sparkles: feat: route to list all users

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
+  Route.get('/users', 'Api/v1/UsersController.index')
   Route.post('/users', 'Api/v1/UsersController.create')
 }).prefix('/api/v1')
