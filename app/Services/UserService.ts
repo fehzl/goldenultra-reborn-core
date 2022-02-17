31351a0ab0f7bcd435d1c37d5abbd08cb6764b1a
Felipe Feliciano <fehzl@live.com>
2022-02-17 00:58:26 +0000

    :sparkles: feat: add create method to UserService

--- app/Services/UserService.ts
+++ app/Services/UserService.ts
@@ -1,6 +1,27 @@
 import { inject, Ioc } from '@adonisjs/core/build/standalone'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import UserRepository from 'App/Repositories/UserRepository'
+import CreateUserValidator from 'App/Validators/User/CreateUserValidator'
 
 @inject()
-export default class UserService {}
+export default class UserService {
+  private UserRepository: UserRepository
+
+  constructor(userRepository: UserRepository) {
+    this.UserRepository = userRepository
+  }
+
+  public async createUser(data: CreateUserValidator['schema']['props']): Promise<any> {
+    const user = await this.UserRepository.create(data)
+
+    return {
+      success: true,
+      httpCode: 201,
+      message: 'User created successfully',
+      data: user,
+    }
+  }
+}
 
 new Ioc().make(UserService)
