6ad7fbfb0cc1814628a093a1c5735edc49aeb0e5
Felipe Feliciano <fehzl@live.com>
2022-02-17 00:40:41 +0000

    :sparkles: feat: create User validator

---
+++ app/Validators/User/CreateUserValidator.ts
@@ -0,0 +1,26 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CreateUserValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    email: schema.string({}, [
+      rules.email(),
+      rules.maxLength(128),
+      rules.unique({ table: 'users', column: 'email' }),
+    ]),
+    passowrd: schema.string({}, [rules.minLength(6), rules.maxLength(128)]),
+  })
+
+  public messages = {
+    'email.required': 'E-mail é obrigatório',
+    'email.email': 'E-mail inválido',
+    'email.maxLength': 'E-mail deve ter no máximo 128 caracteres',
+    'email.unique': 'E-mail já cadastrado',
+
+    'password.required': 'Senha é obrigatória',
+    'password.minLength': 'Senha deve ter no mínimo 6 caracteres',
+    'password.maxLength': 'Senha deve ter no máximo 128 caracteres',
+  }
+}
