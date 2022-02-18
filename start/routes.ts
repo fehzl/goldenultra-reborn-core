0c028a0eed9d88ac17f8bb20c02bff5ed24afdec
Felipe Feliciano <fehzl@live.com>
2022-02-18 20:53:14 +0000

    :sparkles: feat: route to sign in and sign out

--- start/routes.ts
+++ start/routes.ts
@@ -3,4 +3,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('/users', 'Api/v1/UsersController.index')
   Route.post('/users', 'Api/v1/UsersController.create')
+  Route.post('/login', 'Api/v1/AuthController.login')
+  Route.post('/logout', 'Api/v1/AuthController.logout')
 }).prefix('/api/v1')
