8d8a84c5650a2814b25f4082baab2fc2b8a0d51d
Felipe Feliciano <fehzl@live.com>
2022-02-18 20:51:10 +0000

    :sparkles: feat: create Auth controller

---
+++ app/Controllers/Http/Api/v1/AuthController.ts
@@ -0,0 +1,39 @@
+import { inject, Ioc } from '@adonisjs/core/build/standalone'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { Exception } from '@poppinss/utils'
+import BaseController from 'App/Controllers/BaseController'
+import AuthService from 'App/Services/AuthService'
+import AuthValidator from 'App/Validators/Auth/AuthValidator'
+
+@inject()
+export default class AuthController extends BaseController {
+  private authService: AuthService
+
+  constructor(authService: AuthService) {
+    super()
+    this.authService = authService
+  }
+
+  public async login(ctx: HttpContextContract) {
+    const payload = await ctx.request.validate(AuthValidator)
+    const result = await this.authService.login(payload.email, payload.password, ctx)
+
+    if (!result.success && result.error) {
+      throw new Exception(result.message, result.httpCode, result.error.code)
+    }
+
+    return this.send(ctx, result.data, result.message, result.httpCode)
+  }
+
+  public async logout(ctx: HttpContextContract) {
+    const result = await this.authService.logout(ctx)
+
+    if (!result.success && result.error) {
+      throw new Exception(result.message, result.httpCode, result.error.code)
+    }
+
+    return this.send(ctx, result.data, result.message, result.httpCode)
+  }
+}
+
+new Ioc().make(AuthController)
