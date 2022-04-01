7a39e117dacd4a74f2190d61fe83eb18b4146188
Felipe Feliciano <fehzl@live.com>
2022-04-01 20:59:40 +0000

    :sparkles: feat: add delete method to OrderPaymentsController

--- app/Controllers/Http/Api/v1/OrdersPaymentsController.ts
+++ app/Controllers/Http/Api/v1/OrdersPaymentsController.ts
@@ -24,6 +24,16 @@ export default class OrdersPaymentsController extends BaseController {
 
     return this.send(ctx, result.body, result.message, result.httpCode)
   }
+
+  public async delete(ctx: HttpContextContract) {
+    const result = await this.orderPaymentService.delete(ctx.params.id)
+
+    if (!result.success && result.error) {
+      throw new Exception(result.message, result.httpCode, result.error.code)
+    }
+
+    return this.send(ctx, result.body, result.message, result.httpCode)
+  }
 }
 
 new Ioc().make(OrdersPaymentsController)
