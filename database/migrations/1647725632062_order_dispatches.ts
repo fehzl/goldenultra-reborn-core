5176e7a629bd0e80f51ce873a67d84e02b594b48
Felipe Feliciano <fehzl@live.com>
2022-04-01 21:08:43 +0000

    :recycle: refactor: update migrations

---
+++ database/migrations/1647725632062_order_dispatches.ts
@@ -0,0 +1,17 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class OrderDispatches extends BaseSchema {
+  protected tableName = 'order_dispatches'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.raw('uuid_generate_v4()'))
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
