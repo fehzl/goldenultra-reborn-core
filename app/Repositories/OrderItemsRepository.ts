3e220ed05bff8a9f6cab03b1b6a355ab451583ca
Felipe Feliciano <fehzl@live.com>
2022-04-01 21:03:10 +0000

    :sparkles: feat: add separate method

--- app/Repositories/OrderItemsRepository.ts
+++ app/Repositories/OrderItemsRepository.ts
@@ -1,5 +1,7 @@
 import OrderItem from 'App/Models/OrderItem'
+import CreateOrderItemSeparationValidator from 'App/Validators/Order/CreateOrderItemSeparationValidator'
 import CreateOrderItemValidator from 'App/Validators/Order/CreateOrderItemValidator'
+import { DateTime } from 'luxon'
 
 export default class OrderItemsRepository {
   private OrderItem: typeof OrderItem
@@ -22,5 +24,26 @@ export default class OrderItemsRepository {
 
   public async create(data: CreateOrderItemValidator['schema']['props']): Promise<OrderItem> {
     return this.OrderItem.create(data)
+  }
+
+  public async separate(
+    data: CreateOrderItemSeparationValidator['schema']['props'],
+    userId: string
+  ): Promise<OrderItem> {
+    const orderItem = await this.OrderItem.find(data.id)
+
+    if (!orderItem) {
+      throw new Error('Order item not found')
+    }
+
+    console.log('aqui', userId)
+
+    orderItem.amountSeparated = orderItem.amount
+    orderItem.separated = true
+    orderItem.separatedByEmployeeId = userId
+    orderItem.separatedAt = DateTime.now()
+    await orderItem.save()
+
+    return orderItem
   }
 }
